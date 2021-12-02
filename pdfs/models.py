287f1d5bccfa998f96530b383fefc80233d2d6b7
Marco Albero <marco@mybi.es>
2021-12-02 11:46:09 +0100

    First model

--- pdfs/models.py
+++ pdfs/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+# generate sql -> python manage.py makemigrations ->  pdfs/migrations/0001_initial.py
+# run sql on db -> python manage.py migrate
+class Pdf(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(default="")
+    pages = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_created=True)
