01d6041e2517e4fa050f116e62b4d12fa281be56
Marco Albero <marco@mybi.es>
2021-12-02 16:56:42 +0100

    Added random post add request

--- requests.sh
+++ requests.sh
@@ -3,6 +3,16 @@
 
 IFS='|'
 
+if [ "x$1" == "x-pr" ]; then
+    DATA=$(curl -s https://random-data-api.com/api/restaurant/random_restaurant)
+    TITLE=$(echo $DATA | jq '.name')
+    DESC=$(echo $DATA | jq '.description')
+    PAGES=10
+    REQ=$(echo http --raw \'{\"title\": "$TITLE", \"description\": "$DESC", \"pages\": $PAGES}\' POST http://localhost:8000/api/pdfs/)
+    sh -c $REQ
+    exit 0
+fi
+
 if [ "x$1" == "x-l" ]; then
     cat requests.txt | grep -P '^R\d+' | cut -d'#' -f2
 else
