427a469eea58032521f4bf11efc9a3ad1830ff10
Marco Albero <marco@mybi.es>
2021-12-02 12:30:00 +0100

    Rest accessing basic model data

--- pdfs/api/views.py
+++ pdfs/api/views.py
@@ -1,9 +1,10 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from pdfs.models import Pdf
 
 
 class PostApiView(APIView):
     def get(self, request):
-        data = {'name': 'Marco Albero', 'languages': ['python', 'java', 'react']}
+        data = [pdf.title for pdf in Pdf.objects.all()]
         return Response(status=status.HTTP_200_OK, data=data)
