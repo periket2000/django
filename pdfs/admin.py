0b3d909895a2b165be87556f666618564bed9c96
Marco Albero <marco@mybi.es>
2021-12-02 12:03:40 +0100

    Register a model in python admin

--- pdfs/admin.py
+++ pdfs/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from pdfs.models import Pdf
 
 
+# make it appear in http://localhost:8000/admin manager
 @admin.register(Pdf)
 class PdfAdmin(admin.ModelAdmin):
     list_display = ['title', 'created_at']
